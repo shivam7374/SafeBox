d655816d744b2196e59283da86bd5430920c3018
shivam7374 <shivamsingla491@gmail.com>
2020-12-12 20:07:18 +0530

    Website

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('ul li a').forEach((link) => {
-    function scrollTo(e) {
-      e.preventDefault;
-      const id = e.target.getAttribute('href');
-      document.querySelector(`${id}`).scrollIntoView();
-    }
-    link.addEventListener('click', scrollTo);
-  })
-  
