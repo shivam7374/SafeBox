d74459403bf25a3096d0df7a420056d81a39f893
shivam7374 <shivamsingla491@gmail.com>
2020-12-14 09:03:32 +0530

    Update

--- Code.ino
+++ Code.ino
@@ -1,19 +1,14 @@
-// SAFEBOX USING KNOCK PATTERN
-// HARSH-2019UCO1504  SHIVAM-2019UCO1526  YASH-2019UCO1530  HEMANT-2019UCO1534
-
-#include <Servo.h> // Importing the servo library
+#include “Servo.h” // Importing the servo library
 Servo servo_motor; // Creating a servo instance
 
 // Variables defining pin no. for LEDs' , piezo and switch
 const int switch_push = 2;
-const int led_yellow = 3;
 const int led_green = 4;
 const int led_red = 5;
 const int piezo_sensor = A0;
 
-int knock_read;                 // Variable to read piezo value
-int switch_read;                // Variable to read switch value
 const int threshold_knock = 50; // Variable declaring lower limit of knock value i.e threshold value
+int knock_read;                 // Variable to read piezo value
 bool safebox_lock = false;      // Status of safebox lock
 
 void setup()
@@ -21,7 +16,6 @@ void setup()
       servo_motor.attach(9); // Attaching servo motor to 9th pin
       // Configuring the LED pins to behave as output pins
       pinMode(led_red, OUTPUT);
-      pinMode(led_yellow, OUTPUT);
       pinMode(led_green, OUTPUT);
       pinMode(switch_push, INPUT);   // Configuring the switch pin as input pin
       Serial.begin(9600);            // Setting data rate for serial transmission, 9600 being the baud rate
@@ -34,15 +28,15 @@ void loop()
 {
       if (safebox_lock == false) // Checking if safebox is unlocked or not
       {
-            switch_read = digitalRead(switch_push);
+            int switch_read = digitalRead(switch_push);
             if (switch_read == HIGH) // Locking the box if button is pressed
             {
-                  safebox_lock = true;
+                  safebox_lock = true; // Variable to read switch value
                   // Changing the status of LEDs
                   digitalWrite(led_green, LOW);
                   digitalWrite(led_red, HIGH);
                   servo_motor.write(90); // Setting the servo angle to 90 degrees i.e safebox is locked
-                  Serial.println("!! Safeboxbox is locked");
+                  Serial.println("!! Safebox is locked");
                   delay(1000);
             }
       }
@@ -75,22 +69,19 @@ void loop()
       }
 }
 
-int is_knock_valid(int kvalue) // Checking if knock is valid or not
+int is_knock_valid(int knock_value) // Checking if knock is valid or not
 {
-      if (kvalue > threshold_knock)
+      if (knock_value > threshold_knock)
       {
-            // Yellow LED blinks on valid knock
-            digitalWrite(led_yellow, HIGH);
             delay(50);
-            digitalWrite(led_yellow, LOW);
             Serial.print("The knock was valid with value : ");
-            Serial.println(kvalue);
+            Serial.println(knock_value);
             return 1;
       }
       else
       {
             Serial.print("The knock was invalid with value : ");
-            Serial.println(kvalue);
+            Serial.println(knock_value);
             return 0;
       }
 }
